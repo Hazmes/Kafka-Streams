deb0f5a66229ab599b77679c522855418182b707
Hazmes <d.hessmer@googlemail.com>
2018-03-11 21:06:19 +0100

    started building architecture for propper config handling

--- kafkaStreams/src/main/java/dhbw/twitterConn/TwitterStreamConn.java
+++ kafkaStreams/src/main/java/dhbw/twitterConn/TwitterStreamConn.java
@@ -1,4 +1,4 @@
-package dhbw.kafkaStreams;
+package dhbw.twitterConn;
 
 import java.awt.Event;
 import java.util.List;
@@ -23,36 +23,29 @@ import com.twitter.hbc.httpclient.auth.OAuth1;
 public class TwitterStreamConn 
 {
     public static void main( String[] args ) throws InterruptedException
-    {
-        System.out.println( "Hello World!" );
-        
+    {        
         /** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
         BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
         BlockingQueue<Event> eventQueue = new LinkedBlockingQueue<Event>(1000);
 
         /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
         //STREAM_HOSTS : https://stream.twitter.com
-        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        Hosts hosts = new HttpHosts(Constants.STREAM_HOST);
         StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
         // Optional: set up some followings and track terms
-        List<Long> followings = Lists.newArrayList(1234L, 566788L);
+        //followings: Twitter UserId and an L after it --> Why L?
+        //My own Twitter ID: 3834719313L
+        List<Long> followings = Lists.newArrayList(3834719313L);
+        //a phrase will match if all of the terms in the phrase are present in the Tweet
+        //Terms or Followings have to be set. If nothing is set, the connection gets rejected (406)
         List<String> terms = Lists.newArrayList("twitter", "api");
-        hosebirdEndpoint.followings(followings);
-        hosebirdEndpoint.trackTerms(terms);
+     
 
         // These secrets should be read from a config file
-        Authentication hosebirdAuth = new OAuth1("G6gJYlHP3qxvT6lQT1UtzfAsm", "vr5FSQUdXOJHb2npjJE9xyxN4m0qMkIbmhiZGVdZK5wIeM8Obp", "3834719313-W1e7tddRJ35TgFaLlD8BueaczKNprK7U0QRCTna", "dbbUi2bFdXsniK9nQkEjciqU4cC4FH6ewZnyLEZaWRCvR");
+        Authentication auth = new OAuth1("G6gJYlHP3qxvT6lQT1UtzfAsm", "vr5FSQUdXOJHb2npjJE9xyxN4m0qMkIbmhiZGVdZK5wIeM8Obp", "3834719313-W1e7tddRJ35TgFaLlD8BueaczKNprK7U0QRCTna", "dbbUi2bFdXsniK9nQkEjciqU4cC4FH6ewZnyLEZaWRCvR");
      
-        ClientBuilder builder = new ClientBuilder()
-		  		  .name("Hosebird-Client-01")                              // optional: mainly for the logs
-		  		  .hosts(hosebirdHosts)
-		  		  .authentication(hosebirdAuth)
-		  		  .endpoint(hosebirdEndpoint)
-		  		  .processor(new StringDelimitedProcessor(msgQueue));    
-	
-	                     
-
-		Client hosebirdClient = builder.build();
+        Client hosebirdClient = new TwitterClient().build("TestName", hosts, followings, terms, auth, msgQueue);
+        
 		// Attempts to establish a connection.
 		hosebirdClient.connect();
 		
