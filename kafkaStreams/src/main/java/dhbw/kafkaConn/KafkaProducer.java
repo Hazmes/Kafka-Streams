1da9addcdbd3243b9aafcab3dbfca57b3255df54
Hazmes <d.hessmer@googlemail.com>
2018-03-16 12:53:52 +0100

    added setParameters method for config loading

--- kafkaStreams/src/main/java/dhbw/kafkaConn/KafkaProducer.java
+++ kafkaStreams/src/main/java/dhbw/kafkaConn/KafkaProducer.java
@@ -5,6 +5,8 @@ import java.util.Properties;
 
 import org.apache.log4j.Logger;
 
+import com.fasterxml.jackson.databind.annotation.JsonAppend.Prop;
+
 import dhbw.config.Parameter;
 
 public class KafkaProducer {
@@ -13,21 +15,40 @@ public class KafkaProducer {
 	private static final String KEY_SERIALIZER = "key.serializer";
 	private static final String VALUE_SERIALIZER = "value.serializer";
 	private static final String TOPIC = "topic";
+	private KafkaProducer<String,String> producer;
 	Properties props;
 	
 	public KafkaProducer() {
 		props = new Properties();
 	}
 	
+	public void putMessage() {
+		
+	}
+	
 	public void setParameters(List<Parameter> params) {
 		for(Parameter param : params) {
 			switch(param.getKey()) {
-			
-			}
-			if(param.getKey().equals(BOOTSTRAP_SERVERS)) {
-				
+			case BOOTSTRAP_SERVERS:
+				props.put(BOOTSTRAP_SERVERS, param.getValue());
+				break;
+			case KEY_SERIALIZER:
+				props.put(KEY_SERIALIZER, param.getValue());
+				break;
+			case VALUE_SERIALIZER:
+				props.put(VALUE_SERIALIZER, param.getValue());
+				break;
+			case TOPIC:
+				props.put(TOPIC, param.getValue());
+				break;
+			default:
+				log.error("Could not load Config Properties into " + this.getClass().getName() + "\n Failed Property: " + param.getKey());
 			}
 		}
+	}
+	
+	public void close() throws Exception {
+		producer.close();
 	}
 	
 }
