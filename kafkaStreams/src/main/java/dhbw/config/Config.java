2ca4034d92a03fff3a04cf519401530ecfd6ec28
Hazmes <d.hessmer@googlemail.com>
2018-04-09 14:07:34 +0200

    Changed Config Object to create the Kafka Producer Class

--- kafkaStreams/src/main/java/dhbw/config/Config.java
+++ kafkaStreams/src/main/java/dhbw/config/Config.java
@@ -2,12 +2,15 @@ package dhbw.config;
 
 import org.apache.log4j.Logger;
 
+import dhbw.kafkaConn.Producer;
+import dhbw.kafkaConn.ProducerWrapper;
 import dhbw.twitterConn.TwitterClient;
 import dhbw.twitterConn.TwitterClientWrapper;
 
 public class Config {
 	private static Config instance = null;
 	private TwitterClient twitterClient;
+	private Producer kafkaProducer;
 	public String name;
 	public static Logger log = Logger.getLogger(Config.class.getPackage().getName());
 	
@@ -40,7 +43,24 @@ public class Config {
 		}
 	}
 	
+	public void setProducer (ProducerWrapper producerWrapper) throws Exception{
+		try {
+			@SuppressWarnings("unchecked")
+			Class<Producer> producerClass = (Class<Producer>) Class.forName(producerWrapper.getName());
+			this.kafkaProducer = (Producer) producerClass.newInstance();
+			this.kafkaProducer.setParameters(producerWrapper.getParameters());
+			this.kafkaProducer.init();
+		}catch(Exception e) {
+			throw e;
+		}
+	}
+	
+	public Producer getProducer() {
+		return kafkaProducer;
+	}
 	public TwitterClient getTwitterClient() {
 		return twitterClient;
 	}
+	
+	
 }
